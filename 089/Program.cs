5a48929200d4e58c9bec38fc9806b4a8141ea06a
Dmitrian23 <dimonio1991@mail.ru>
2023-02-25 21:49:11 +0300

    Задача 089

---
+++ 089/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача_№89 Подсчитать сумму цифр, встречающихся в строке
+
+ string n="Text 12 PRiveT 2";
+  char s='0';
+  int sum=0;
+  
+for (int i = 0; i < n.Length; i++)
+{
+     s=n[i];
+    System.Console.Write($"{s}"); 
+if(s>=48&&s<=57)
+{
+   sum += n[i] - 48;  
+}
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма цифр встречающихся в строке = ( {sum} )");
+
+
+
