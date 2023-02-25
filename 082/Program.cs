4f6f7e3ebc16e04ccc45409e09ecc2c78fc3e29f
Dmitrian23 <dimonio1991@mail.ru>
2023-02-25 21:46:50 +0300

    Задача 082

---
+++ 082/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача_№82 C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+
+string s="Jojopa."; // string? s=Console.ReadLine();
+int countBefore = s.IndexOf('.');
+System.Console.WriteLine(countBefore);
+
+// string s = "qwerty.qwey";
+// int countBefore = s.IndexOf('.');
+// int countAfter = s.Length - countBefore - 1;
+// System.Console.WriteLine(countAfter);
+// System.Console.WriteLine(countBefore);
+
+
