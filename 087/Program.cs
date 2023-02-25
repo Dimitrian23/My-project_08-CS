a514063054e42eba78226981479205e3c2afbb5e
Dmitrian23 <dimonio1991@mail.ru>
2023-02-25 21:48:47 +0300

    Задача 087

---
+++ 087/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача_№87 Удалить средний символ при нечетной длине строки и две средние буквы при четной длине строки
+
+string s="ABCCDE";// string? s=Console.ReadLine();
+if (s.Length%2==0)
+  s=s.Remove(s.Length/2-1,2);
+  else
+    s=s.Remove(s.Length/2,1);
+System.Console.WriteLine(s);
+
+
+
+
+
+
+
+
