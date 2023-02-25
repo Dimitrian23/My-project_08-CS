3e3eced4a6ff6a563a63f1fa3ff810bd7026d014
Dmitrian23 <dimonio1991@mail.ru>
2023-02-25 21:47:02 +0300

    Задача 083

---
+++ 083/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача_№83 Подсчитать количество цифр среди вводимых с клавиатуры символов
+
+string s="asdfg122";// string? s=Console.ReadLine();
+int k=0;
+// for(int i=0;i<s.Length;i++)
+//   if(s[i]>='0'&& s[i]<='9') k++;
+
+// foreach(char c in s)
+// if(c>='0'&& c<='9') k++;
+
+foreach(char c in s)
+if(char.IsDigit(c)) k++;
+System.Console.WriteLine(k);
+
+
+
+
+
+
