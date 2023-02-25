b26c344238a4be04c67a86d007c8d5d814bf7951
Dmitrian23 <dimonio1991@mail.ru>
2023-02-25 21:48:21 +0300

    Задача 085

---
+++ 085/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача_№85  Преобразовать во вводимой с клавиатуры строке строчные латинские буквы в прописные
+
+string s="asdплвasdфывфывasskjhjdfidddsa"; // string? s=Console.ReadLine();
+
+System.Text.StringBuilder sb=new System.Text.StringBuilder(s);
+for(int i=0;i<sb.Length;i++)
+  if (sb[i]>='a' && sb[i]<='z')
+    sb[i]=char.ToUpper(s[i]);
+s=sb.ToString();
+System.Console.WriteLine(s);   
+
+
+
